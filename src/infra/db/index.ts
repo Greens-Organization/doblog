c3d9090112e8949f50860837c9c437a9ee468f43
AlphaLawless <alpha.vylly12@gmail.com>
2025-05-20 17:30:25 -0300

    refactor: replace Neon database integration with PostgreSQL in migration and initialization logic

--- src/infra/db/index.ts
+++ src/infra/db/index.ts
@@ -1,20 +1,8 @@
-import { env, isDevelopment } from '@/env'
+import { env } from '@/env'
 import * as schema from '@/infra/db/schema'
-import { neon } from '@neondatabase/serverless'
-import { drizzle as drizzleNeon } from 'drizzle-orm/neon-http'
-import { drizzle as drizzlePostgres } from 'drizzle-orm/node-postgres'
+import { drizzle } from 'drizzle-orm/node-postgres'
 
-const clientNeon = neon(env.DATABASE_URL)
-
-const drizzle = {
-  neon: drizzleNeon(clientNeon, {
-    schema,
-    logger: env.DRIZZLE_LOGGER
-  }),
-  default: drizzlePostgres(env.DATABASE_URL, {
-    schema,
-    logger: env.DRIZZLE_LOGGER
-  })
-}
-
-export const db = isDevelopment ? drizzle.default : drizzle.neon
+export const db = drizzle(env.DATABASE_URL, {
+  schema,
+  logger: env.DRIZZLE_LOGGER
+})
