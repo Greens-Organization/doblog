a82675aaa0da4217d53c469da641ea18ef241e73
Matheus P Agostinho <contact@matheuspa.com>
2025-05-20 19:01:08 -0300

    feat: include blog schemas in the main export for better organization

--- src/infra/db/schema.ts
+++ src/infra/db/schema.ts
@@ -2,11 +2,21 @@ import { account } from './schemas/auth/account'
 import { session } from './schemas/auth/session'
 import { user } from './schemas/auth/user'
 import { verification } from './schemas/auth/verification'
+import { category } from './schemas/blog/category'
+import { post } from './schemas/blog/post'
+import { subcategory } from './schemas/blog/subcategory'
+import { tag } from './schemas/blog/tag'
 
 export const schemas = {
   // better auth
   ...user,
   ...session,
   ...account,
-  ...verification
+  ...verification,
+
+  // blog
+  ...post,
+  ...category,
+  ...subcategory,
+  ...tag
 }
