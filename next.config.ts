1c5d1f426f88d175f9a8de4d6711fccabe8468f6
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-05-23 15:02:47 -0400

    fix: update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import { isDevelopment } from '@/env'
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  devIndicators: { position: 'bottom-right' },
   experimental: {
     nodeMiddleware: true
   },
