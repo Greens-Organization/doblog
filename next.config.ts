9dc3153a187398ef8e0b6b3630e0ede40cfece5f
Matheus P Agostinho <contact@matheuspa.com>
2025-05-25 12:27:23 -0300

    fix(next.config.ts): update webpack configuration to handle node modules
    - Added fallback for 'fs' module to prevent errors in development.
    - Updated externals configuration to include 'node:fs', 'node:crypto', and other necessary packages for compatibility.

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
-import { isDevelopment } from '@/env'
-import type { NextConfig } from 'next'
+import { isDevelopment } from '@/env';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   experimental: {
@@ -9,7 +9,33 @@ const nextConfig: NextConfig = {
     fetches: {
       fullUrl: isDevelopment
     }
-  }
+  },
+  webpack(
+    config,
+    // { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack },
+  ) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+
+      fs: false, // the solution
+    };
+
+    // NOTE: Must use this instead of serverExternalPackages | serverComponentsExternalPackages
+    // because node:crypto for some reason only works below.
+    // Also, when the below is defined, it overrides the serverExternalPackages,
+    // so pino and pino-pretty must be included here.
+    config.externals.push({
+      'node:fs': 'commonjs fs',
+      'node:crypto': 'commonjs crypto',
+      argon2: 'argon2',
+      pino: 'pino',
+      'thread-stream': 'thread-stream',
+      'pino-worker': 'pino-worker',
+      'pino-file': 'pino-file',
+      'pino-pretty': 'pino-pretty',
+    });
+    return config;
+  },
 }
 
 export default nextConfig
