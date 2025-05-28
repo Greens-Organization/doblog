b624c20ae80a14e23ed9222ca440f2b18bc1f29b
Matheus P Agostinho <contact@matheuspa.com>
2025-05-27 21:31:52 -0300

    Add POST route handler for creating posts

---
+++ src/app/api/v1/blog/post/route.ts
@@ -0,0 +1,5 @@
+import { createPost } from '@/core/blog/post/services'
+import { createApiHandler } from '@/infra/helpers/handlers/api/create-api-handler'
+
+export const POST = createApiHandler(createPost)
+// export const GET = createApiHandler(listSubcategories)
