bfaa1c0575b0c052c6085556166767d2281c3172
Matheus P Agostinho <contact@matheuspa.com>
2025-05-31 12:47:21 -0300

    Add migration for setting NOT NULL constraints on timestamps and seed data for posts

--- src/infra/db/schemas/helpers.ts
+++ src/infra/db/schemas/helpers.ts
@@ -1,7 +1,10 @@
 import { text, timestamp } from 'drizzle-orm/pg-core'
 
 export const idPrimaryKey = text('id')
+  .notNull()
   .primaryKey()
   .$defaultFn(() => crypto.randomUUID())
-export const createdAt = timestamp('created_at').defaultNow()
-export const updatedAt = timestamp('updated_at').$onUpdate(() => new Date())
+export const createdAt = timestamp('created_at').notNull().defaultNow()
+export const updatedAt = timestamp('updated_at')
+  .notNull()
+  .$onUpdate(() => new Date())
