0c567276ac41da559b8261aba5d5c7effc37a912
Matheus P Agostinho <contact@matheuspa.com>
2025-06-08 19:17:48 -0300

    feat: extend generateRandomURLAvatar function to support additional avatar type and custom seed

--- src/infra/helpers/dicebear.ts
+++ src/infra/helpers/dicebear.ts
@@ -1,9 +1,13 @@
 interface Options {
-  type?: 'glass' | 'lorelei-neutral'
+  customSeed?: string
+  type?: 'glass' | 'lorelei-neutral' | 'notionists'
 }
 
-function generateRandomURLAvatar({ type = 'glass' }: Options = {}): string {
-  const id = crypto.randomUUID().slice(0, 8)
+function generateRandomURLAvatar({
+  type = 'glass',
+  customSeed
+}: Options = {}): string {
+  const id = customSeed ?? crypto.randomUUID().slice(0, 8)
 
   const url = `https://api.dicebear.com/9.x/${type}/svg?seed=${id}`
   return url
