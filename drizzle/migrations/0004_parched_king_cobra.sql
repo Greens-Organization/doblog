b6849aae9f75d482fafe4334e82ac74f5480c1dd
Matheus P Agostinho <contact@matheuspa.com>
2025-06-10 11:41:48 -0300

    Remove obsolete migration snapshot and update journal with new migration tag

--- drizzle/migrations/0004_parched_king_cobra.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "member" ALTER COLUMN "created_at" SET DEFAULT now();--> statement-breakpoint
-ALTER TABLE "organization" ALTER COLUMN "created_at" SET DEFAULT now();--> statement-breakpoint
-ALTER TABLE "organization" ADD COLUMN "description" text;--> statement-breakpoint
-ALTER TABLE "organization" ADD COLUMN "keywords" text;--> statement-breakpoint
-ALTER TABLE "organization" ADD COLUMN "updated_at" timestamp NOT NULL;
