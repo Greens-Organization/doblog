cda8f6adf8cdd899914ac7bff9f20e7b5fa0e215
Matheus P Agostinho <contact@matheuspa.com>
2025-06-13 15:44:28 -0300

    feat: make name, email, and role fields optional in updateUserSchema

--- src/infra/validations/schemas/user/update.validation.ts
+++ src/infra/validations/schemas/user/update.validation.ts
@@ -18,9 +18,10 @@ export const updateUserSchema = () => {
     name: zod
       .string({ error: 'Name must be a valid string' })
       .min(1, { error: 'The name must have at least 1 character' })
-      .max(255, { error: 'The name must have at most 255 characters' }),
-    email: zod.email('Email must be a valid email address'),
-    role: zod.enum(UserRole, 'Role must be a valid user role')
+      .max(255, { error: 'The name must have at most 255 characters' })
+      .optional(),
+    email: zod.email('Email must be a valid email address').optional(),
+    role: zod.enum(UserRole, 'Role must be a valid user role').optional()
   })
 }
 
