85abe3edf60bae748cd933e86e7ec27a7711fe27
Matheus P Agostinho <contact@matheuspa.com>
2025-06-24 13:03:05 -0300

    Refactor database migrations: remove obsolete migration file, update snapshot, and create new migration for initial schema setup
    - Deleted the old migration file `0001_aspiring_barracuda.sql`.
    - Updated the snapshot file to reflect the new migration ID and structure.
    - Removed the previous snapshot file to clean up the migration history.
    - Created a new migration file `0000_regular_sharon_ventura.sql` to establish the initial database schema, including tables for account, invitation, member, organization, session, user, user_to_category, verification, category, post, subcategory, and tag.
    - Added foreign key constraints to ensure referential integrity across the new tables.

--- drizzle/migrations/0001_aspiring_barracuda.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "user_to_category" DROP CONSTRAINT "user_to_category_user_id_user_id_fk";
---> statement-breakpoint
-ALTER TABLE "post" DROP CONSTRAINT "post_author_id_user_id_fk";
---> statement-breakpoint
-ALTER TABLE "user_to_category" ADD CONSTRAINT "user_to_category_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "post" ADD CONSTRAINT "post_author_id_user_id_fk" FOREIGN KEY ("author_id") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
