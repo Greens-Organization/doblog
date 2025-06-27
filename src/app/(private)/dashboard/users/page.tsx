22a219ce1e069af54336302c8a1dd20f541c6846
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-06-27 16:03:39 -0400

    refactor: update users page to use listUsers action
    Signed-off-by: Kaik Selhorst <oficial.kaikselhorst@gmail.com>

--- src/app/(private)/dashboard/users/page.tsx
+++ src/app/(private)/dashboard/users/page.tsx
@@ -1,54 +1,28 @@
-import { Badge } from '@/components/ui/badge'
-import { Button } from '@/components/ui/button'
+import { listUsers } from '@/actions/dashboard/user'
+import { DefaultError } from '@/components/errors'
 import { Input } from '@/components/ui/input'
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow
 } from '@/components/ui/table'
 import { Search } from 'lucide-react'
-import Link from 'next/link'
 import { DashNavbar } from '../components/dash-navbar'
 import { InviteUser } from './invite-user'
+import { UsersTableRow } from './users-table-row'
 
-export default function UsersPage() {
-  const users = [
-    {
-      id: 1,
-      name: 'Admin',
-      email: 'admin@example.com',
-      role: 'Admin',
-      status: 'Ativo',
-      lastLogin: '15/12/2023'
-    },
-    {
-      id: 2,
-      name: 'João Silva',
-      email: 'joao@example.com',
-      role: 'Editor',
-      status: 'Ativo',
-      lastLogin: '14/12/2023'
-    },
-    {
-      id: 3,
-      name: 'Maria Oliveira',
-      email: 'maria@example.com',
-      role: 'Editor',
-      status: 'Ativo',
-      lastLogin: '13/12/2023'
-    },
-    {
-      id: 4,
-      name: 'Pedro Santos',
-      email: 'pedro@example.com',
-      role: 'Editor',
-      status: 'Inativo',
-      lastLogin: '10/12/2023'
-    }
-  ]
+export default async function UsersPage() {
+  const usersRes = await listUsers()
+
+  if (!usersRes.success) {
+    return (
+      <DefaultError description={usersRes.error} status={usersRes.status} />
+    )
+  }
+
+  const { data: users } = usersRes.data
 
   return (
     <section>
@@ -81,44 +55,16 @@ export default function UsersPage() {
                 <TableHead>Nome</TableHead>
                 <TableHead>Email</TableHead>
                 <TableHead>Função</TableHead>
-                <TableHead>Status</TableHead>
-                <TableHead>Último Login</TableHead>
+                <TableHead>Publicações</TableHead>
+                <TableHead>Rascunhos</TableHead>
                 <TableHead className="text-right">Ações</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
-              {users.map((user) => (
-                <TableRow key={user.id}>
-                  <TableCell className="font-medium">{user.name}</TableCell>
-                  <TableCell>{user.email}</TableCell>
-                  <TableCell>
-                    <Badge
-                      variant={user.role === 'Admin' ? 'default' : 'secondary'}
-                    >
-                      {user.role}
-                    </Badge>
-                  </TableCell>
-                  <TableCell>
-                    <Badge
-                      variant={
-                        user.status === 'Ativo' ? 'outline' : 'destructive'
-                      }
-                    >
-                      {user.status}
-                    </Badge>
-                  </TableCell>
-                  <TableCell>{user.lastLogin}</TableCell>
-                  <TableCell className="text-right">
-                    <div className="flex justify-end gap-2">
-                      <Button variant="ghost" size="sm" asChild>
-                        <Link href={`/admin/users/edit/${user.id}`}>
-                          Editar
-                        </Link>
-                      </Button>
-                    </div>
-                  </TableCell>
-                </TableRow>
-              ))}
+              {users.map((user) => {
+                if (user.email === 'anonymous') return null
+                return <UsersTableRow user={user} key={user.id} />
+              })}
             </TableBody>
           </Table>
         </div>
