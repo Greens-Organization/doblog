b383e6eb2e4e8c672f9e66aa48c1f446b189eedb
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-07-02 14:56:41 -0400

    fix: infer additionalFields in the Auth client

--- src/infra/lib/better-auth/auth-client.ts
+++ src/infra/lib/better-auth/auth-client.ts
@@ -1,10 +1,15 @@
 import { admin, editor } from '@/core/auth/permissions'
 import { UserRole } from '@/core/blog/user/dto'
-import { organizationClient } from 'better-auth/client/plugins'
+import {
+  inferAdditionalFields,
+  organizationClient
+} from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react'
+import type { auth } from './auth'
 
 export const authClient = createAuthClient({
   plugins: [
+    inferAdditionalFields<typeof auth>(),
     organizationClient({
       roles: { [UserRole.ADMIN]: admin, [UserRole.EDITOR]: editor }
     })
