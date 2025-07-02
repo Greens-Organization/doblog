8bb2d1862b1012b04db76b1c1bd0f5d809bdb3fa
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-07-02 15:59:07 -0400

    feat: add new schemas in auth pages

--- src/components/auth-form.tsx
+++ src/components/auth-form.tsx
@@ -1,100 +1,99 @@
 'use client'
+import {
+  DicebearTypes,
+  generateRandomURLAvatar
+} from '@/infra/helpers/dicebear'
 import { authClient } from '@/infra/lib/better-auth/auth-client'
+import { signInSchema, signUpSchema } from '@/infra/validations/schemas/auth'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter, useSearchParams } from 'next/navigation'
-import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
+import { DefaultField } from './form/default-field'
 import { Button } from './ui/button'
-import { Input } from './ui/input'
+import { Form } from './ui/form'
 
 export function SignInForm() {
   const router = useRouter()
   const searchParams = useSearchParams()
-  const [isLoading, setIsLoading] = useState(false)
+  const form = useForm({ resolver: zodResolver(signInSchema) })
 
   const callbackURL = searchParams.get('callbackURL') || '/'
 
   return (
-    <form
-      className="space-y-3"
-      onSubmit={async (e) => {
-        e.preventDefault()
-        setIsLoading(true)
-        const formData = new FormData(e.currentTarget)
+    <Form {...form}>
+      <form
+        className="space-y-3"
+        onSubmit={form.handleSubmit(async (data) => {
+          const { error } = await authClient.signIn.email({
+            email: data.email,
+            password: data.password,
+            callbackURL: callbackURL
+          })
 
-        const email = formData.get('email') || ''
-        const password = formData.get('password') || ''
-
-        const { error } = await authClient.signIn.email({
-          email: email as string,
-          password: password as string,
-          callbackURL: callbackURL
-        })
-
-        setIsLoading(false)
-        if (error) {
-          toast.error(error.message)
-          return
-        }
-        router.push(callbackURL)
-      }}
-    >
-      <Input placeholder="Email" name="email" type="email" required />
-      <Input placeholder="Password" name="password" type="password" required />
-      <Button
-        className="w-full cursor-pointer"
-        type="submit"
-        disabled={isLoading}
+          if (error) {
+            toast.error(error.message)
+            return
+          }
+          router.push(callbackURL)
+        })}
       >
-        Continuar
-      </Button>
-    </form>
+        <DefaultField name="email" placeholder="Email" type="email" />
+        <DefaultField name="password" placeholder="Password" type="password" />
+
+        <Button
+          className="w-full cursor-pointer"
+          type="submit"
+          disabled={form.formState.isSubmitting}
+        >
+          Continuar
+        </Button>
+      </form>
+    </Form>
   )
 }
 
 export function SignUpForm() {
   const router = useRouter()
   const searchParams = useSearchParams()
-  const [isLoading, setIsLoading] = useState(false)
+
+  const form = useForm({ resolver: zodResolver(signUpSchema) })
 
   const callbackURL = searchParams.get('callbackURL') || '/sign-in'
 
   return (
-    <form
-      className="space-y-3"
-      onSubmit={async (e) => {
-        e.preventDefault()
-        setIsLoading(true)
-        const formData = new FormData(e.currentTarget)
+    <Form {...form}>
+      <form
+        className="space-y-3"
+        onSubmit={form.handleSubmit(async (data) => {
+          // TODO: REMOVE ROLE FROM HERE WARNING!!!!!!!!!!!!!!!
+          const { error } = await authClient.signUp.email({
+            email: data.email,
+            name: data.name,
+            password: data.password,
+            role: 'editor',
+            callbackURL: callbackURL,
+            image: generateRandomURLAvatar({ type: DicebearTypes.notionists })
+          })
 
-        const email = formData.get('email') || ''
-        const name = formData.get('name') || ''
-        const password = formData.get('password') || ''
-
-        const { error } = await authClient.signUp.email({
-          email: email as string,
-          name: name as string,
-          password: password as string,
-          callbackURL: callbackURL
-        })
-
-        setIsLoading(false)
-        if (error) {
-          toast.error(error.message)
-          return
-        }
-        router.push(`/sign-up/confirm-account?callbackURL=${callbackURL}`)
-      }}
-    >
-      <Input placeholder="Name" name="name" required />
-      <Input placeholder="Email" name="email" type="email" required />
-      <Input placeholder="Password" name="password" type="password" required />
-      <Button
-        className="w-full cursor-pointer"
-        type="submit"
-        disabled={isLoading}
+          if (error) {
+            toast.error(error.message)
+            return
+          }
+          router.push(`/sign-up/confirm-account?callbackURL=${callbackURL}`)
+        })}
       >
-        Continuar
-      </Button>
-    </form>
+        <DefaultField name="name" placeholder="Name" />
+        <DefaultField name="email" placeholder="Email" type="email" />
+        <DefaultField name="password" placeholder="Password" type="password" />
+        <Button
+          className="w-full cursor-pointer"
+          type="submit"
+          disabled={form.formState.isSubmitting}
+        >
+          Continuar
+        </Button>
+      </form>
+    </Form>
   )
 }
